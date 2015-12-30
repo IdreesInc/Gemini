ebdb2ed5b31812eb041833c0c9b8445c7bec60f8
Idrees Hassan <idrees@idreesinc.com>
2015-12-30 14:32:32 -0500

    Added communication between twins.
    The twins now communicate with one another through the clipboard.

--- src/com/idreesinc/gemini/Gemini.java
+++ src/com/idreesinc/gemini/Gemini.java
@@ -3,44 +3,169 @@
  */
 package com.idreesinc.gemini;
 
+import java.awt.Toolkit;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 public class Gemini {
 
+    private static String name;
+    private static String twinName;
+    private static Path twinPath;
+    private static String waitingFor;
+    private static final String TOKEN = "⬡";
     private static final String SEPERATOR = File.separator;
     private static final int MAX_DISTANCE = 5;
-    private static final int MAX_ATTEMPTS = 5;
+    private static final int MAX_ATTEMPTS = 10;
+    private static final int UPDATE_INTERVAL = 500;
 
     public static void main(String[] args) {
-        int attempts = 0;
-        boolean succeeded = false;
-        while (!succeeded && attempts < MAX_ATTEMPTS) {
-            String path = Gemini.class.getProtectionDomain().getCodeSource().getLocation().toString().replace("file:", "").replace("%20", " ");
-            File newLocation = randomFileLocation(path, MAX_DISTANCE);
-            if (!newLocation.getPath().contains("Gemini.jar")) {
-                newLocation = new File(newLocation.getAbsolutePath() + SEPERATOR + "Gemini.jar");
+        String clipboardText = getClipboardText();
+        if (clipboardText != null && clipboardText.startsWith(TOKEN)) { //If an instance of Gemini is already running
+            if (clipboardText.startsWith(TOKEN + "Apollo")) {
+                name = "Artemis";
+                twinName = "Apollo";
+            } else if (clipboardText.startsWith(TOKEN + "Artemis")) {
+                name = "Apollo";
+                twinName = "Artemis";
+            } else {
+                System.err.println(clipboardText);
+                terminate();
             }
-            if (path.contains("jar")) {
-                try {
-                    Files.move(Paths.get(path), newLocation.toPath(), REPLACE_EXISTING);
-                    succeeded = true;
-                    say("*Poof*" + "\nPssst: " + newLocation.getAbsolutePath());
-                } catch (IOException ex) {
-                    Logger.getLogger(Gemini.class.getName()).log(Level.SEVERE, null, ex);
-                    succeeded = false;
+            if (clipboardText.contains("Waiting for twin")) {
+                sendClipboardMessage(name + " activated");
+                waitingFor = "Path";
+            }
+            System.out.println(name + " activated");
+        } else { //If this is the first instance of Gemini
+            name = "Artemis";
+            twinName = "Apollo";
+            twinPath = null;
+            int attempts = 0;
+            while (twinPath == null && attempts < MAX_ATTEMPTS) {
+                twinPath = duplicate();
+                attempts++;
+            }
+            if (twinPath == null) {
+                System.err.println("Unable to generate twin");
+                terminate();
+            } else {
+                System.out.println("Generated twin at " + twinPath);
+            }
+            sendClipboardMessage("Waiting for twin");
+            waitingFor = "activated";
+            try {
+                Runtime.getRuntime().exec(" java -jar " + twinPath.toString());
+            } catch (IOException ex) {
+            }
+        }
+        //Update loop
+        Timer timer = new Timer();
+        timer.schedule(new Update(), 0, UPDATE_INTERVAL);
+    }
+
+    static class Update extends TimerTask {
+
+        @Override
+        public void run() {
+            String clipboardText = getClipboardText();
+            if (clipboardText != null && clipboardText.startsWith(TOKEN + twinName)) {
+                System.out.println(clipboardText);
+                if (waitingFor != null && clipboardText.contains(waitingFor)) {
+                    String[] split = clipboardText.split("=");
+                    switch (waitingFor) {
+                        case "Path": //Generated twin's path has been recieved
+                            twinPath = Paths.get(split[1]);
+                            System.out.println("Twin's path: " + twinPath);
+                            waitingFor = null;
+                            setClipboardText("");
+                            break;
+                        case "activated": //Generated twin has been created
+                            sendClipboardMessage("Path=" + Gemini.class.getProtectionDomain().getCodeSource().getLocation().toString().replace("file:", "").replace("%20", " "));
+                            waitingFor = null;
+                            break;
+                    }
                 }
             }
-            attempts++;
+            if (clipboardText != null && clipboardText.startsWith(TOKEN) && clipboardText.contains("Terminate")) {
+                terminate();
+            }
         }
+    }
+
+    /**
+     * Returns the text stored in the system's clipboard.
+     *
+     * @return The text stored in the clipboard
+     */
+    private static String getClipboardText() {
+        String text = "";
+        try {
+            text = (String) Toolkit.getDefaultToolkit().getSystemClipboard().getData(DataFlavor.stringFlavor);
+        } catch (UnsupportedFlavorException | IOException ex) {
+        }
+        return text;
+    }
+
+    /**
+     * Stores the given text into the system's clipboard.
+     *
+     * @param text The text to store
+     */
+    private static void setClipboardText(String text) {
+        StringSelection selection = new StringSelection(text);
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection, selection);
+        System.out.println(text);
+    }
+
+    /**
+     * Sets the clipboard's text as a combination of the globally specified
+     * token, this twin's name, a colon with a space, and the message given (in
+     * that order).
+     *
+     * @param message The message to add to the clipboard
+     */
+    private static void sendClipboardMessage(String message) {
+        setClipboardText(TOKEN + name + ": " + message);
+    }
+
+    /**
+     * Attempts to duplicate this jar to a random location.
+     *
+     * @return The path of the duplicated jar
+     */
+    private static Path duplicate() {
+        String path = Gemini.class.getProtectionDomain().getCodeSource().getLocation().toString().replace("file:", "").replace("%20", " ");
+        Path duplicatedPath = null;
+        File newLocation = randomFileLocation(path, MAX_DISTANCE);
+        if (!newLocation.getPath().contains("Gemini.jar")) {
+            newLocation = new File(newLocation.getAbsolutePath() + SEPERATOR + "Gemini.jar");
+        }
+        if (path.contains("jar")) {
+            try {
+                Files.copy(Paths.get(path), newLocation.toPath(), REPLACE_EXISTING);
+                duplicatedPath = newLocation.toPath();
+                System.out.println("Duplicated to " + newLocation.getAbsolutePath());
+            } catch (IOException ex) {
+                Logger.getLogger(Gemini.class.getName()).log(Level.SEVERE, null, ex);
+                duplicatedPath = null;
+            }
+        }
+        return duplicatedPath;
     }
 
     /**
@@ -53,7 +178,7 @@ public class Gemini {
      * within
      * @return A file with the calculated path
      */
-    public static File randomFileLocation(String currentPath, int verticalDistance) {
+    private static File randomFileLocation(String currentPath, int verticalDistance) {
         Random rand = new Random();
         int folderLevel = currentPath.length() - currentPath.replace(SEPERATOR, "").length();
         int newFolderLevel = folderLevel;
@@ -84,15 +209,15 @@ public class Gemini {
      * @param path The path containing the folders
      * @return A randomized folder
      */
-    public static String getRandomFolderInPath(String path) {
+    private static String getRandomFolderInPath(String path) {
         File file = new File(path);
         String[] names = file.list();
         ArrayList<String> folders = new ArrayList<>();
-        for (String name : names) {
-            if (new File(path + SEPERATOR + name).isDirectory() && !name.contains("%")) {
-                folders.add(path + SEPERATOR + name);
-                if (name.equalsIgnoreCase("Torch")) {
-                    return path + SEPERATOR + name;
+        for (String nm : names) {
+            if (new File(path + SEPERATOR + nm).isDirectory() && !nm.contains("%")) {
+                folders.add(path + SEPERATOR + nm);
+                if (nm.equalsIgnoreCase("Torch")) {
+                    return path + SEPERATOR + nm;
                 }
             }
         }
@@ -110,6 +235,14 @@ public class Gemini {
      * @param message The message to output
      */
     private static void say(String message) {
+        System.out.println(name + " said: " + message);
         JOptionPane.showMessageDialog(null, message, "Gemini", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    /**
+     * Terminates the application.
+     */
+    private static void terminate() {
+        System.exit(0);
     }
 }
